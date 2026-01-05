20b98c64e4d7dbbd1a0af67a83e2fe7b3de0a77d
elijahschlecht6767 <elijahschlecht6767@gmail.com>
2026-01-05 08:53:28 -0500

    Automated_teller_machine_machine

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    welcome!
+
+    (sign in prompt with acc num and pin, q to exit imediately)
+
+    please do something (enter num)
+    1)switch accounts
+    2)deposit
+    3)withdraw
+    4)exit atm
+    """
+
+
+if __name__=="__main__":
+    main()
