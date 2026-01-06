30f5a3f49024312a8207608047c67d1c03d95b37
elijahschlecht6767 <elijahschlecht6767@gmail.com>
2026-01-06 10:16:47 -0500

    fixed acc and pin confirmation and menu

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import json
+import os
 
 def lookfornum(asi):
     with open("accounts.json", "r") as jsonfile:
         data = json.load(jsonfile)
         for i in range(len(data)):
-            if data[i]["accountNumber"]==asi:
+            if int(data[i]["accountNumber"])==asi:
                 return i
+        print(len(data))
         return None
 
 
 def main():
+    brreak=0
     notfound=0
-    try:
-        with open ("accounts.json" "r") as diddy:
-            data0=json.load(diddy)
-    except:
+    if os.path.exists("accounts.json"):
+        notfound=0
+    else:
         print("accounts file not found")
         notfound=1
     if notfound==0:
         while True:
+            if brreak==1:
+                break
             try:
                 asi=int(input("Enter account number: -"))
                 datanum=lookfornum(asi)
+                print(datanum)
                 if datanum!=None:
                     while True:
                         try:
                             pin=int(input("Enter pin number: -"))
                             with open("accounts.json", "r") as jsonfile:
                                 data = json.load(jsonfile)
-                            if data[datanum]["pin"]==pin:
+                            if int(data[datanum]["pin"])==pin:
+                                brreak=1
                                 break
                             else:
                                 print("invalid pin number")
@@ -38,6 +44,13 @@ def main():
                     print("invalid account number")
             except:
                 print("invalid account number")
+    if notfound==0:
+        print("""Please select a number for the action you want performed
+    1) Deposit
+    2) Withdraw
+    3) View balance
+    4) Exit ATM program
+    """)
         #continue here
 
 
