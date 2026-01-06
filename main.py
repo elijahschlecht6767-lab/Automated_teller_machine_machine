8ab0e1b18e14eeac3a823e805f51d201f8762d34
elijahschlecht6767 <elijahschlecht6767@gmail.com>
2026-01-06 09:48:59 -0500

    acc and pin confirmation

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+import json
+
+def lookfornum(asi):
+    with open("accounts.json", "r") as jsonfile:
+        data = json.load(jsonfile)
+        for i in range(len(data)):
+            if data[i]["accountNumber"]==asi:
+                return i
+        return None
+
+
 def main():
-    """
-    welcome!
-
-    (sign in prompt with acc num and pin, q to exit imediately)
-
-    please do something (enter num)
-    1)switch accounts
-    2)deposit
-    3)withdraw
-    4)exit atm
-    """
+    notfound=0
+    try:
+        with open ("accounts.json" "r") as diddy:
+            data0=json.load(diddy)
+    except:
+        print("accounts file not found")
+        notfound=1
+    if notfound==0:
+        while True:
+            try:
+                asi=int(input("Enter account number: -"))
+                datanum=lookfornum(asi)
+                if datanum!=None:
+                    while True:
+                        try:
+                            pin=int(input("Enter pin number: -"))
+                            with open("accounts.json", "r") as jsonfile:
+                                data = json.load(jsonfile)
+                            if data[datanum]["pin"]==pin:
+                                break
+                            else:
+                                print("invalid pin number")
+                        except:
+                            print("invalid pin number")
+                else:
+                    print("invalid account number")
+            except:
+                print("invalid account number")
+        #continue here
 
 
 if __name__=="__main__":
