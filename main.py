aadc3766573e108b4d79558067076c83e312a529
elijahschlecht6767 <elijahschlecht6767@gmail.com>
2026-01-07 09:09:26 -0500

    worked on menu choices and finished depositing

--- main.py
+++ main.py
@@ -1,57 +1,106 @@
 import json
 import os
 
-def lookfornum(asi):
-    with open("accounts.json", "r") as jsonfile:
-        data = json.load(jsonfile)
+def deposit(data, account):
+    while True:
+        try:
+            print("Which account do you want to deposit to?\nPlease select a number for the action you want performed\n1) Checking\n2) Savings")
+            choice=int(input("-"))
+            if choice==1:
+                balance="checkingBalance"
+                baltyp="Checking"
+            elif choice==2:
+                balance="savingsBalance"
+                baltyp="Savings"
+            elif choice<1 or choice>2:
+                print("Invalid number, please try again")
+                continue
+            amm=float(input("Enter the ammount you would like to deposit (without $ sign) -"))
+            data[account]["transactions"].append(f"Deposit into {baltyp} in amount of ${amm:.2f}")
+            data[account][balance]+=amm
+            with open("accounts.json", "w") as json_file:
+                json.dump(data, json_file, indent=4)
+        except:
+            print("Error")
+        print("Invalid number, please try again")
+
+
+def menu_pick(data, account):
+    while True:
+        try:
+            choice=int(input("-"))
+            if choice==1:
+                deposit(data, account)
+                return False
+            elif choice==2:
+                withdraw(data, account)#unfinished
+                return False
+            elif choice==3:
+                view_balance(data, account)#unfinished
+                return False
+            elif choice==4:
+                return True
+        except:
+            print("Error")
+        print("invalid choice, please try again")
+
+
+def get_acc(acc_num, pin, data):
         for i in range(len(data)):
-            if int(data[i]["accountNumber"])==asi:
-                return i
-        print(len(data))
+            if data[i]["accountNumber"] == acc_num:
+                if data[i]["pin"]==pin:
+                    return i
+                else:
+                    break
         return None
 
 
-def main():
-    brreak=0
-    notfound=0
-    if os.path.exists("accounts.json"):
-        notfound=0
-    else:
-        print("accounts file not found")
-        notfound=1
-    if notfound==0:
-        while True:
-            if brreak==1:
+def look_for_data(item, data, data_type, account):
+        if data[account][data_type] == item:
+                return True
+        return False
+
+
+def load_file():
+    try:
+        with open ("accounts.json", "r") as file:
+            data = json.load(file)
+    except Exception:
+        print("File Not Found")
+        return []
+    return data
+
+
+def sign_in(data):
+    while True:
+        try:
+            acc_num = input("Enter account number: ")
+            pin_num = input("Enter PIN number: ")
+            account = get_acc(acc_num, pin_num, data)
+            if account!=None:
                 break
-            try:
-                asi=int(input("Enter account number: -"))
-                datanum=lookfornum(asi)
-                print(datanum)
-                if datanum!=None:
-                    while True:
-                        try:
-                            pin=int(input("Enter pin number: -"))
-                            with open("accounts.json", "r") as jsonfile:
-                                data = json.load(jsonfile)
-                            if int(data[datanum]["pin"])==pin:
-                                brreak=1
-                                break
-                            else:
-                                print("invalid pin number")
-                        except:
-                            print("invalid pin number")
-                else:
-                    print("invalid account number")
-            except:
-                print("invalid account number")
-    if notfound==0:
-        print("""Please select a number for the action you want performed
-    1) Deposit
-    2) Withdraw
-    3) View balance
-    4) Exit ATM program
-    """)
-        #continue here
+        except:
+            print("Error")
+        print("invalid account/PIN number, please try again")
+    return account
+
+
+def main():
+    data = load_file()
+    if data != []:
+        account = sign_in(data)
+        while True:
+            print("""Please select a number for the action you want performed
+        1) Deposit
+        2) Withdraw
+        3) View balance
+        4) Exit ATM program
+        """)
+            exit=menu_pick(data, account)
+            if exit==True:
+                break
+    else:
+        print("Accounts File Not Found.")
 
 
 if __name__=="__main__":
